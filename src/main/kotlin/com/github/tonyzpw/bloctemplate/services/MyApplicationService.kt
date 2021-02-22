813e149da09c0b5adee13a66d5dc4c8e05f05f42
GitHub Action <action@github.com>
2021-02-22 04:38:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/tonyzpw/bloctemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/tonyzpw/bloctemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.tonyzpw.bloctemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tonyzpw.bloctemplate.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/tonyzpw/bloctemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tonyzpw/bloctemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.tonyzpw.bloctemplate.services
 
+import com.github.tonyzpw.bloctemplate.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
